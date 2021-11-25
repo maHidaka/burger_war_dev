585a861375c9fd2094315715f195165b76501313
maHidaka <50083939+maHidaka@users.noreply.github.com>
2021-11-25 12:54:17 +0900

    敵検出プログラムの追加

--- burger_war_dev/scripts/enemy_detect.py
+++ burger_war_dev/scripts/enemy_detect.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy
+import numpy as np
 import random
 
-from geometry_msgs.msg import Twist
 
+from geometry_msgs.msg import Twist
+from obstacle_detector.msg import Obstacles
 import tf
 
 
@@ -12,78 +14,31 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib_msgs
 
+
 import yaml
 import os
+import roslib.packages
 
-from cv_bridge import CvBridge, CvBridgeError
+# Ref: https://hotblackrobotics.github.io/en/blog/2018/01/29/action-client-py/
+
+#from std_msgs.msg import String
+#from sensor_msgs.msg import Image
+#from cv_bridge import CvBridge, CvBridgeError
 import cv2
-from sensor_msgs.msg import Image
-import numpy as np
+
 
 
 
 class NaviBot():
     def __init__(self):
-        waypoint_file = rospy.get_param('/waypoint_file_path','/home/handaru/catkin_ws/src/burger_war_dev/burger_war_dev/scripts/waypoints.yaml')
+        path = roslib.packages.get_pkg_dir('burger_war_dev') + '/scripts/waypoints.yaml'
+        waypoint_file = rospy.get_param('/waypoint_file_path',path)
 
         # velocity publisher
         self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
+        self.obstacles = rospy.Subscriber("obstacles", Obstacles, self.detect)
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.waypoints = self.waypoint_load(waypoint_file)
-        
-        self.img = None
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('image_raw', Image, self.imageCallback)
-
-    def imageCallback(self, data):
-        try:
-            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            rospy.logerr(e)
-
-        lim = []
-        lim.append([340, 18, 230, 140])  # 赤
-        circles, scale = self.find_target(self.img, lim)
-        cv2.imshow("Image window", scale)
-        cv2.waitKey(1)
-
-    def find_target(self,image, lim):
-        # hsv空間の作成
-        limit = lim[0]
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
-        h = hsv[:, :, 0]
-        s = hsv[:, :, 1]
-        # 取得画像のサイズでhsv空間の配列を作成
-        mask = np.zeros(h.shape, dtype=np.uint8)
-        # 表示用にmaskと同じサイズの配列を作成
-        hsv_scale = np.copy(mask)
-        # hsvのしきい値で二値化
-        if limit[0] > limit[1]:
-            mask[((h > limit[0] * (255/360)) | (h < limit[1] * (255/360)))
-                & ((limit[3] < s) & (s < limit[2]))] = 255
-        else:
-            mask[((h > limit[0] * (255/360)) & (h < limit[1] * (255/360)))
-                & ((limit[3] < s) & (s < limit[2]))] = 255
-        # 輪郭計算
-        _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        circles = []
-        # 計算した輪郭をなめらかにマルっと包み込む
-        for contour in contours:
-            approx = cv2.convexHull(contour)
-            area = cv2.contourArea(approx)
-            # デカすぎor小さすぎな領域はスキップ
-            if area < 1e2 or 1e5 < area:
-                continue
-            # 最小外接円で近似
-            (x, y), radius = cv2.minEnclosingCircle(approx)
-            # 返り値準備
-            center = (int(x), int(y))
-            radius = int(radius)
-            circles.append(np.array((center, radius)))
-            hsv_scale = mask
-        return circles, hsv_scale
-
 
 
     def waypoint_load(self, file_path):
@@ -94,6 +49,52 @@ class NaviBot():
 
             return waypoints
 #            print(yml['waypoints'][0])
+
+    def detect(self, data):
+        for c in data.circles:
+
+
+            if -0.3 < c.center.x < 0.3 :
+                if -0.3 < c.center.y < 0.3 :
+                    print("center")
+                    print("-"*5)
+                    continue
+            if -0.7 < c.center.x < -0.4 :
+                if -0.7 < c.center.y < -0.3 :
+                    print("r_back")
+                    print("-"*5)
+                    continue
+            if -0.7 < c.center.x < -0.4 :
+                if 0.4 < c.center.y < 0.7 :
+                    print("l_back")
+                    print("-"*5)
+                    continue
+            if 0.3 < c.center.x < 0.6 :
+                if -0.7 < c.center.y < -0.4 :
+                    print("r_front")
+                    print("-"*5)
+                    continue
+            if 0.3 < c.center.x < 0.7 :
+                if 0.3 < c.center.y < 0.7 :
+                    print("l_front")
+                    print("-"*5)
+                    continue
+
+            if -0.8 < c.center.x*0.707 < 0.8 :
+                if -0.8 < c.center.y*0.707 < 0.8 :
+                    print("in range")
+                else :
+                    print("out")
+                    continue
+            else :
+                print("out")
+                continue
+            
+            print(c.center)
+            print("-"*5)
+        print("-"*20)
+        if not c == "":
+            self.setGoal(c.center.x,c.center.y,0)
 
 
     def setGoal(self,x,y,yaw):
@@ -122,12 +123,13 @@ class NaviBot():
 
 
     def strategy(self):
-        r = rospy.Rate(5) # change speed 5fps
+        r = rospy.Rate(1) # change speed 5fps
 
         while not rospy.is_shutdown():
             for pos in self.waypoints['waypoints']:
                 print(pos['x'])
-                self.setGoal(pos['x'],pos['y'],pos['th'])
+                #self.setGoal(pos['x'],pos['y'],pos['th'])
+
 
 
 
